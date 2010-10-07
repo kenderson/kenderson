aa27ca3348414e5eeac3947d836c01a35cdc0240
Ken Henderson <kenderson_MBP@kenderson-mbp.local>
2010-10-07 12:40:24 -0400

    fixing up the IE script

--- public/javascripts/ie.js
+++ public/javascripts/ie.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function($) {
-  // jQuery.each(jQuery.browser, function(i) {
-  //   if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)){ //test for MSIE x.x;
-  //     var ieversion=new Number(RegExp.$1) // capture x.x portion and store as a number
-  //     if (ieversion < 9){
-  //       $("#ie_flash").wait(2000).slideDown(1000);
-  //     }
-  //   }
-  // });
+  jQuery.each(jQuery.browser, function(i) {
+    if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)){ //test for MSIE x.x;
+      var ieversion=new Number(RegExp.$1) // capture x.x portion and store as a number
+      if (ieversion < 9){
+        $("#ie_flash").wait(2000).slideDown(1000);
+      }
+    }
+  });
 });
