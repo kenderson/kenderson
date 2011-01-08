df434de0de2b9445c627013bf8d3bfef6fd8def6
Ken Henderson <kenderson_MBP@kenderson-mbp.local>
2011-01-08 12:59:30 -0500

    touches to the menu

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -32,7 +32,24 @@ $.fn.wait = function(time, type) {
   };
 })(jQuery);
 
+
 jQuery(document).ready(function($) {
+  $(function(){
+    var active_menu = $("#navigation li a.current").attr("id");
+    $("#"+active_menu+"_arrow").fadeIn(500);
+  });
+  $("#navigation li a:not(#navigation li a.current)").hover(
+    function () {
+      $("#"+this.id+"_arrow").stop(true, true).fadeIn(500);
+      var active_menu = $("#navigation li a.current").attr("id");
+      $("#"+active_menu+"_arrow").stop(true, true).fadeOut(500);
+    }, 
+    function () {
+      $("#"+this.id+"_arrow").stop(true, true).fadeOut(500);
+      var active_menu = $("#navigation li a.current").attr("id");
+      $("#"+active_menu+"_arrow").stop(true, true).fadeIn(500);
+    }
+  );
 	GetTwitterFeedIncRT('kenderson', 1, 'twitter_update_list');
   $('#contact_tabs').cycle({
       fx:     'scrollRight',
@@ -50,124 +67,125 @@ jQuery(document).ready(function($) {
        speed:  'slow',
        timeout: 7000
    });
-  jQuery.each(jQuery.browser, function(i) {
-    if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)){ //test for MSIE x.x;
-      var ieversion=new Number(RegExp.$1) // capture x.x portion and store as a number
-      if (ieversion < 9 ){
-        $.fn.qtip.styles.mystyle = { 
-          padding: 0, 
-          border: { color: '#ffaf00', radius: 7 },
-          'background':'#ffaf00',
-          color: 'black',
-          textAlign: 'center',
-          tip: 'bottomMiddle',
-          width: { min: 165 }
-        }
-      }
-      else {
-        $.fn.qtip.styles.mystyle = { 
-          padding: 0, 
-          border: { color: '#ffaf00', width: 0 },
-          'background':'transparent',
-          color: 'black',
-          textAlign: 'center',
-          tip: 'bottomMiddle',
-          width: { min: 135 }
-        }
-      }
-    }
-    else {
-      $.fn.qtip.styles.mystyle = { 
-        padding: 0, 
-        border: { color: '#ce8429', width: 0 },
-        'background':'transparent',
-        color: 'black',
-        textAlign: 'center',
-        tip: 'bottomMiddle',
-        width: { min: 135 }
-      }
-    }
-  });
-  $("a.fancybox").fancybox({
-    	'autoScale'		: false,
-      'width' : 500,
-  		'transitionIn'		: 'elastic',
-  		'transitionOut'		: 'elastic',
-  		'titlePosition' 	: 'over'
-  });
-  $('[tooltip]').each(function() {
-    $(this).qtip({
-      content: $(this).attr('tooltip'),
-      position: {
-        adjust: { resize: true, screen: false },
-        corner: {
-          target: 'topRight',
-          tooltip: 'bottomLeft'
-        }
-      },
-      style: {
-        border: {
-          width: 2,
-          radius: 10
-        },
-        textAlign: 'center',
-        tip: 'bottomLeft', 
-        name: 'light'
-      }
-    });
-  });
-  
-  $('a.fancybox').live("click", function(){
-    $.fancybox($("#"+this.href.split("#")[1]).html());
-  });
-  $('#navigation li a').each(function() {
-    if ($(this).hasClass("current")) {
-      $(this).qtip({
-        content: $(this).attr('tooltip_menu'),
-        show: { ready: true},
-        hide: false,
-        position: {
-          adjust: { resize: true, screen: false },
-          corner: {
-            target: 'topMiddle',
-            tooltip: 'bottomMiddle'
-          }
-        },
-        style: 'mystyle'
-      });
-    }
-    else {
-      $(this).qtip({    
-        content: $(this).attr('tooltip_menu'),
-        position: {
-          corner: {
-            target: 'topMiddle',
-            tooltip: 'bottomMiddle'
-          }
-        },
-        style: 'mystyle',
-        api: {
-          beforeShow: function() {
-            $(".qtip").hide();
-          },
-          beforeHide: function() {
-            $('#navigation li a.current').qtip({    
-              content: $('#navigation li a.current').attr('tooltip_menu'),
-              show: { ready: true},
-              hide: false,
-              position: {
-                corner: {
-                  target: 'topMiddle',
-                  tooltip: 'bottomMiddle'
-                }
-              },
-              style: 'mystyle'
-            });
-          }
-        }
-      });
-    }
-  });
+   $("a.fancybox").fancybox({
+     	'autoScale'		: false,
+       'width' : 500,
+   		'transitionIn'		: 'elastic',
+   		'transitionOut'		: 'elastic',
+   		'titlePosition' 	: 'over'
+   });
+   $('a.fancybox').live("click", function(){
+     $.fancybox($("#"+this.href.split("#")[1]).html());
+   });
+   
+  // jQuery.each(jQuery.browser, function(i) {
+  //   if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)){ //test for MSIE x.x;
+  //     var ieversion=new Number(RegExp.$1) // capture x.x portion and store as a number
+  //     if (ieversion < 9 ){
+  //       $.fn.qtip.styles.mystyle = { 
+  //         padding: 0, 
+  //         border: { color: '#ffaf00', radius: 7 },
+  //         'background':'#ffaf00',
+  //         color: 'black',
+  //         textAlign: 'center',
+  //         tip: 'bottomMiddle',
+  //         width: { min: 165 }
+  //       }
+  //     }
+  //     else {
+  //       $.fn.qtip.styles.mystyle = { 
+  //         padding: 0, 
+  //         border: { color: '#ffaf00', width: 0 },
+  //         'background':'transparent',
+  //         color: 'black',
+  //         textAlign: 'center',
+  //         tip: 'bottomMiddle',
+  //         width: { min: 135 }
+  //       }
+  //     }
+  //   }
+  //   else {
+  //     $.fn.qtip.styles.mystyle = { 
+  //       padding: 0, 
+  //       border: { color: '#ce8429', width: 0 },
+  //       'background':'transparent',
+  //       color: 'black',
+  //       textAlign: 'center',
+  //       tip: 'bottomMiddle',
+  //       width: { min: 135 }
+  //     }
+  //   }
+  // });  
+  // $('[tooltip]').each(function() {
+  //   $(this).qtip({
+  //     content: $(this).attr('tooltip'),
+  //     position: {
+  //       adjust: { resize: true, screen: false },
+  //       corner: {
+  //         target: 'topRight',
+  //         tooltip: 'bottomLeft'
+  //       }
+  //     },
+  //     style: {
+  //       border: {
+  //         width: 2,
+  //         radius: 10
+  //       },
+  //       textAlign: 'center',
+  //       tip: 'bottomLeft', 
+  //       name: 'light'
+  //     }
+  //   });
+  // });
+  // 
+  // $('#navigation li a').each(function() {
+  //   if ($(this).hasClass("current")) {
+  //     $(this).qtip({
+  //       content: $(this).attr('tooltip_menu'),
+  //       show: { ready: true},
+  //       hide: false,
+  //       position: {
+  //         adjust: { resize: true, screen: false },
+  //         corner: {
+  //           target: 'topMiddle',
+  //           tooltip: 'bottomMiddle'
+  //         }
+  //       },
+  //       style: 'mystyle'
+  //     });
+  //   }
+  //   else {
+  //     $(this).qtip({    
+  //       content: $(this).attr('tooltip_menu'),
+  //       position: {
+  //         corner: {
+  //           target: 'topMiddle',
+  //           tooltip: 'bottomMiddle'
+  //         }
+  //       },
+  //       style: 'mystyle',
+  //       api: {
+  //         beforeShow: function() {
+  //           $(".qtip").hide();
+  //         },
+  //         beforeHide: function() {
+  //           $('#navigation li a.current').qtip({    
+  //             content: $('#navigation li a.current').attr('tooltip_menu'),
+  //             show: { ready: true},
+  //             hide: false,
+  //             position: {
+  //               corner: {
+  //                 target: 'topMiddle',
+  //                 tooltip: 'bottomMiddle'
+  //               }
+  //             },
+  //             style: 'mystyle'
+  //           });
+  //         }
+  //       }
+  //     });
+  //   }
+  // });
   $("#kenderson").validate();
   // $('#facebox').bgiframe();
   // $("a.facebox").facebox();
